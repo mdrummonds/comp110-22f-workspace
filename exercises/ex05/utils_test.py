d3f5d22559c4ef504be0437735562fb3aff12cda
Madi Drummonds <mdrummonds@unc.edu>
2022-09-25 15:31:53 -0400

    FIxed Docstring Errors

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,45 +1,53 @@
-"""tests for utils functions only_evens, concat, and sub."""
+"""Tests for utils functions only_evens, concat, and sub."""
 __author__ = "730482673"
+
 
 from utils import only_evens
 from utils import sub
 from utils import concat
 
+
 def test_only_evens_empty() -> None:
-    """tests what happens with an empty list."""
+    """Tests what happens with an empty list."""
     odds_and_evens: list[int] = []
     assert only_evens(odds_and_evens) == []
 
+
 def test_only_evens_random_nums() -> None:
-    """tests with regular numbers."""
+    """Tests with regular numbers."""
     odds_and_evens: list[int] = [1, 2, 3, 4, 5, 6]
     assert only_evens(odds_and_evens) == [2, 4, 6]
 
+
 def test_only_evens_negative_nums() -> None:
-    """tests with negative numbers."""
+    """Tests with negative numbers."""
     odds_and_evens: list[int] = [-4, -2, 0, 2, 4]
     assert only_evens(odds_and_evens) == [-4, -2, 0, 2, 4]
 
+
 def test_concat_empty() -> None:
-    """tests when given two empty lists."""
+    """Tests when given two empty lists."""
     first_list: list[int] = []
     second_list: list[int] = []
     assert concat(first_list, second_list) == []
 
+
 def test_concat_randoms_nums() -> None:
-    """tests when given two regular lists."""
+    """Tests when given two regular lists."""
     first_list: list[int] = [1, 2, 3]
     second_list: list[int] = [1, 2, 3]
     assert concat(first_list,second_list) == [1, 2, 3, 1, 2, 3]
 
+
 def test_concat_negative_nums() -> None:
-    """tests when given two lists with negative numbers."""
+    """Tests when given two lists with negative numbers."""
     first_list: list[int] = [-1, -2 , -3]
     second_list: list[int] = [-3, -2, -1]
     assert concat(first_list, second_list) == [-1, -2, -3, -3, -2, -1]
 
+
 def test_sub_negative_start() -> None:
-    """tests when given a negative start index."""
+    """Tests when given a negative start index."""
     input_list: list[int] = [1, 2, 3, 4]
     start_index: int = -1
     end_index: int = 4
@@ -47,7 +55,7 @@ def test_sub_negative_start() -> None:
 
 
 def test_sub_random_nums() -> None:
-    """tests when given regular numbers."""
+    """Tests when given regular numbers."""
     input_list: list[int] = [2, 7, 42, 6]
     start_index: int = 1
     end_index: int = 4
@@ -55,7 +63,7 @@ def test_sub_random_nums() -> None:
 
 
 def test_sub_rand_int() -> None:
-    """tests when given a list with negative numbers."""
+    """Tests when given a list with negative numbers."""
     input_list: list[int] = [-1, 2, 6, 7]
     start_index: int = 0
     end_index: int = 3
