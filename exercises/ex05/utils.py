6ae856ae495a2bb64400107c7d317a8e61421885
Madi Drummonds <mdrummonds@unc.edu>
2022-09-25 12:24:52 -0400

    Created Utils and Tests for Utils

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,56 @@
+"""Building list utilities functions."""
+
+__author__ = "730482673"
+
+def only_evens(odds_and_evens: list[int]) -> list[int]:
+    """Returns even numbers given in a list."""
+    i: int = 0
+    even_squad: list[int] = []
+    while i < len(odds_and_evens):
+        if odds_and_evens[i] % 2 == 0:
+            even_squad.append(odds_and_evens[i])
+        i += 1
+    return even_squad
+
+
+def concat(first_list: list[int], second_list: list[int]) -> list[int]:
+    """Takes two lists and combines them."""
+    third_list: list[int] = []
+    i: int = 0
+    while i < len(first_list):
+        third_list.append(first_list[i])
+        i += 1
+    i = 0
+    while i < len(second_list):
+        third_list.append(second_list[i])
+        i += 1
+    return third_list
+
+
+def sub(input_list: list[int], start_index: int, end_index: int) -> list[int]:
+    """A subset of the given list between the specified start and end values."""
+    i: int = start_index
+    new_list: list[int] = []
+    if start_index < 0:
+        start_index = 0
+    elif end_index > len(input_list):
+        end_index = len(input_list)
+    elif len(input_list) == 0:
+        return input_list
+    while start_index < len(input_list) + 1 and start_index < end_index:
+        new_list.append(input_list[start_index])
+        start_index += 1
+    return new_list
+
+
+    
+
+
+
+            
+
+       
+    
+
+
+
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,62 @@
+"""tests for utils functions only_evens, concat, and sub."""
+__author__ = "730482673"
+
+from utils import only_evens
+from utils import sub
+from utils import concat
+
+def test_only_evens_empty() -> None:
+    """tests what happens with an empty list."""
+    odds_and_evens: list[int] = []
+    assert only_evens(odds_and_evens) == []
+
+def test_only_evens_random_nums() -> None:
+    """tests with regular numbers."""
+    odds_and_evens: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(odds_and_evens) == [2, 4, 6]
+
+def test_only_evens_negative_nums() -> None:
+    """tests with negative numbers."""
+    odds_and_evens: list[int] = [-4, -2, 0, 2, 4]
+    assert only_evens(odds_and_evens) == [-4, -2, 0, 2, 4]
+
+def test_concat_empty() -> None:
+    """tests when given two empty lists."""
+    first_list: list[int] = []
+    second_list: list[int] = []
+    assert concat(first_list, second_list) == []
+
+def test_concat_randoms_nums() -> None:
+    """tests when given two regular lists."""
+    first_list: list[int] = [1, 2, 3]
+    second_list: list[int] = [1, 2, 3]
+    assert concat(first_list,second_list) == [1, 2, 3, 1, 2, 3]
+
+def test_concat_negative_nums() -> None:
+    """tests when given two lists with negative numbers."""
+    first_list: list[int] = [-1, -2 , -3]
+    second_list: list[int] = [-3, -2, -1]
+    assert concat(first_list, second_list) == [-1, -2, -3, -3, -2, -1]
+
+def test_sub_negative_start() -> None:
+    """tests when given a negative start index."""
+    input_list: list[int] = [1, 2, 3, 4]
+    start_index: int = -1
+    end_index: int = 4
+    assert sub(input_list, start_index, end_index) == [1, 2, 3, 4]
+
+
+def test_sub_random_nums() -> None:
+    """tests when given regular numbers."""
+    input_list: list[int] = [2, 7, 42, 6]
+    start_index: int = 1
+    end_index: int = 4
+    assert sub(input_list, start_index, end_index) == [7, 42, 6]
+
+
+def test_sub_rand_int() -> None:
+    """tests when given a list with negative numbers."""
+    input_list: list[int] = [-1, 2, 6, 7]
+    start_index: int = 0
+    end_index: int = 3
+    assert sub(input_list, start_index, end_index) == [-1, 2, 6]
